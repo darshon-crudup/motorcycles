f8fd5c7bd0c85db0d28576a0bcf9762896d264fa
DARSHON CRUDUP <dlc756@yahoo.com>
2023-01-30 00:34:33 -0500

    semi colons

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function Darshon (){
-let introduction = prompt('What\'s your name?')
+let introduction = prompt('What\'s your name?');
 document.write('Hello '+ introduction)
 }
 
 function motorcycle(){
-let likesyamaha = confirm("Would you like to buy a motorcycle?")
+let likesyamaha = confirm("Would you like to buy a motorcycle?");
 
 if (likesyamaha ===true){
-    document.write("Great, I think I found what your looking for!")
+    document.write("Great, I think I found what your looking for!");
 } else {
-    document.write('Not cool')
+    document.write('Not cool');
 }
 }
 
 function ducati(){
-let likesducati = confirm("Do you like this 2022 Ducati Diavel?")
+let likesducati = confirm("Do you like this 2022 Ducati Diavel?");
 
 if (likesducati ===true){
-    document.write("Would you like to purchase it for $1?")
+    document.write("Would you like to purchase it for $1?");
 } else{
-    document.write('I don\'t believe you')
+    document.write('I don\'t believe you');
 }
 function verify(){
 	let verify = prompt("Verify your age!");
